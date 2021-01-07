e0f84543713d31d8cf6a47c07dcbfbd06b03ce2f
vfavorito <vince.favorito@gmail.com>
2021-01-06 20:21:40 -0600

    Created the front end ajax to send data to the back end in burger.js file

---
+++ public/assets/js/burger.js
@@ -0,0 +1,30 @@
+$(function () {
+    $(".change-devoured").on("click", function (event) {
+        let id = $(this).data("id");
+        let newDevoured = $(this).data("newDevoured");
+
+        let newDevouredState = {
+            devoured: newDevoured
+        };
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevouredState
+        }).then(
+            location.reload()
+        );
+    });
+    $(".create-form").on("submit", function (event) {
+        event.preventDefault();
+
+        let newBurger = {
+            burger: $("#burger_name").val().trim(),
+            devoured: false
+        };
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            location.reload()
+        );
+    });
+});
