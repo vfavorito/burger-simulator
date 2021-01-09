33edf39926c6911a87d1d818acec1294f428d0c3
vfavorito <vince.favorito@gmail.com>
2021-01-08 18:56:11 -0600

    Changed connection variable to allow use of JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 const mysql = require("mysql");
+const connection;
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+    });
+}
 connection.connect((error) => {
     if (error) {
         console.error("error connecting: " + err.stack);
